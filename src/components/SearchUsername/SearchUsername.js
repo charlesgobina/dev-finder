e6401c10ff507cd6751cd06ca50da7954c2dda80
charlesgobina <charles.gobina@gmail.com>
2022-07-11 23:50:32 +0100

    Bind search form

--- src/components/SearchUsername/SearchUsername.js
+++ src/components/SearchUsername/SearchUsername.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BiSearch } from 'react-icons/bi';
 import './SearchUsername.css';
 
-const SearchUsername = () => (
-  <section className="searchBarContainer">
-    <div className="userSearch">
-      <BiSearch className="searchIcon" />
-      <input type="text" placeholder="Search GitHub username..." />
-      <button type="button">Search</button>
-    </div>
-  </section>
+const SearchUsername = () => {
+  const [search, setSearch] = useState('');
 
-);
+  return (
+    <section className="searchBarContainer">
+      <div className="userSearch">
+        <BiSearch className="searchIcon" />
+        <form>
+          <input type="text" onChange={(e) => setSearch(e.target.value)} value={search} placeholder="Search GitHub username..." />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+    </section>
+  );
+};
 
 export default SearchUsername;
