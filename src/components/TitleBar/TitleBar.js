29fd8d1ea9127b42f1f8dd56b8d1a1fffa6f5b26
charlesgobina <charles.gobina@gmail.com>
2025-01-31 09:55:57 +0200

    chore: bug fixes

--- src/components/TitleBar/TitleBar.js
+++ src/components/TitleBar/TitleBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { BiSun, BiMoon } from 'react-icons/bi';
+import { BiSun } from 'react-icons/bi';
+import { Tooltip } from 'react-tooltip';
 import './TitleBar.css';
 
 const TitleBar = ({ setLightMode, lightMode }) => {
@@ -9,13 +10,18 @@ const TitleBar = ({ setLightMode, lightMode }) => {
   };
 
   return (
-    <div className="titleBar">
+    <div className="titleBar" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
       <h1>devfinder</h1>
       <div className="themeSwitch">
-        <p>{ lightMode ? 'light' : 'dark' }</p>
-        <button onClick={handleClick} type="button">
-          { lightMode ? <BiSun size={25} /> : <BiMoon size={25} /> }
-        </button>
+        <a data-tooltip-id="my-tooltip" href="#tooltip" data-tooltip-content="Coming Soon">
+          <BiSun size={25} />
+        </a>
+        <p>{lightMode ? 'light' : 'dark'}</p>
+        <Tooltip id="my-tooltip">
+          <button onClick={handleClick} type="button">
+            {/* {lightMode ? <BiSun size={25} /> : <BiMoon size={25} />} */}
+          </button>
+        </Tooltip>
       </div>
     </div>
   );
